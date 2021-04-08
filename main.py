7358b43f7903a5ec9676b65db6d8438562303abf
Carson Boles <>
2021-04-08 18:02:08 +0000

    Average

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-answer=input('what is your favorite animal?')
-print('Your favorite animal is a Crocodile')
+answer=int(input("enter a number"))
+answer2=int(input("enter a second number"))
+answer3=int(input("enter a third number"))
+answer4=int(input("enter a fourth number"))
+answer5=int(input("enter a fifth number"))
+average=(answer+answer2+answer3+answer4+answer5)/5
+print(average)
